f299f20c41e295464c731f376cab6ef248800da8
lee <g7huway0310@gmail.com>
2020-07-20 20:52:52 +0800

    Initial Commit

---
+++ OpenPeeps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenPeeps
+//
+//  Created by guowei on 2020/7/20.
+//  Copyright © 2020 guowei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
